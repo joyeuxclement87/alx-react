4d964a3fcd1fd8cb98e17a5b33913dc56345f1c6
joyeuxclement87 <tuyishimirejoyeuxclement@gmail.com>
2024-11-21 22:35:56 +0200

    working with fix rotate fromjs

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,4 +1,4 @@
-// working with fromjs
+// import { fromJS } from 'immutable';
 import { fromJS } from './node_modules/immutable/dist/immutable';
 
 const getImmutableObject = (object) => fromJS(object);
